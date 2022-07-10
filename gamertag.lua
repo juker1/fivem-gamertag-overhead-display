2522723bf23bdb9d2636ffb91ebad8d74d354363
Juker <69339219+juker1@users.noreply.github.com>
2022-07-10 13:03:33 -0700

    Revamped
    Much better now and works for everybody

--- gamertag.lua
+++ gamertag.lua
@@ -1,52 +1,53 @@
 CreateThread(function()
+    local gamerTags = {}
+
     while true do
         Wait(0)
-        local playerId = PlayerId()
-        local Ped = GetPlayerPed(-1)
-        local PlayerName = GetPlayerName(playerId)
-        local WantedLevel = GetPlayerWantedLevel(playerId)
-        local CurrentVehicle = GetVehiclePedIsIn(PlayerPedId())
-        
-        if NetworkIsPlayerActive(playerId) and playerId == playerId then
+        for _, p in ipairs(GetActivePlayers()) do
+            local CurrentPlayer = PlayerId()
+            local ped = GetPlayerPed(p)
+            local WantedLevel = GetPlayerWantedLevel(p)
+            local CurrentVehicle = GetVehiclePedIsIn(ped)
 
-            if IsMpGamerTagActive(GamerTagId) then
-	            RemoveMpGamerTag(GamerTagId)
-	            repeat Wait(0) until IsMpGamerTagFree(GamerTagId)
+            if p ~= CurrentPlayer and NetworkIsPlayerActive(CurrentPlayer) then
+					
+                    if not IsMpGamerTagActive(gamerTags[p]) then
+                        gamerTag = CreateFakeMpGamerTag(ped, GetPlayerName(p), false, false, "", 0)
+                    end
+
+                    SetMpGamerTagVisibility(gamerTags[p], 2, IsPlayerTargettingEntity(CurrentPlayer, ped))
+                    SetMpGamerTagVisibility(gamerTags[p], 4, NetworkIsPlayerTalking(p))
+
+                    -- Wanted Level
+                    if WantedLevel > 0 then
+                        SetMpGamerTagVisibility(gamerTags[p], 7, true)
+                        SetMpGamerTagWantedLevel(gamerTags[p], WantedLevel)
+                    else
+                        SetMpGamerTagVisibility(gamerTags[p], 7, false)
+                    end
+
+                    -- Driver / Passenger
+                    if IsPedSittingInVehicle(ped, CurrentVehicle) and GetPedInVehicleSeat(CurrentVehicle, -1) == ped then
+                        SetMpGamerTagVisibility(gamerTags[p], 8, true)
+                        SetMpGamerTagVisibility(gamerTags[p], 9, false)
+                    elseif IsPedSittingInVehicle(ped, CurrentVehicle) and GetPedInVehicleSeat(CurrentVehicle, 0) == ped then
+                        SetMpGamerTagVisibility(gamerTags[p], 8, false)
+                        SetMpGamerTagVisibility(gamerTags[p], 9, true)
+                    else
+                        SetMpGamerTagVisibility(gamerTags[p], 8, false)
+                        SetMpGamerTagVisibility(gamerTags[p], 9, false)
+                    end
+
+                    SetMpGamerTagAlpha(gamerTags[p], 2, 255)
+                    SetMpGamerTagHealthBarColor(gamerTags[p], 0)
+
+                    SetMpGamerTagColour(gamerTags[p], 0, 0)
+                    SetMpGamerTagColour(gamerTags[p], 4, 0)
+                    SetMpGamerTagAlpha(gamerTags[p], 4, 255)
+                    SetMpGamerTagsUseVehicleBehavior(true)
+
+                    gamerTags[p] = gamerTag
             end
-
-            local GamerTagId = CreateMpGamerTagWithCrewColor(playerId, PlayerName, false, false, "", 0, 0, 0, 0)
-            
-            SetMpGamerTagVisibility(GamerTagId, 0, true)
-            SetMpGamerTagVisibility(GamerTagId, 4, NetworkIsPlayerTalking(playerId))
-            SetMpGamerTagVisibility(GamerTagId, 5, IsPauseMenuActive())
-
-            if WantedLevel > 0 then
-                SetMpGamerTagVisibility(GamerTagId, 7, true)
-                SetMpGamerTagWantedLevel(GamerTagId, WantedLevel)
-            else
-                SetMpGamerTagVisibility(GamerTagId, 7, false)
-            end
-
-            if IsPedSittingInVehicle(Ped, CurrentVehicle) and GetPedInVehicleSeat(CurrentVehicle, -1) == PlayerPedId() then
-                SetMpGamerTagVisibility(GamerTagId, 8, true)
-                SetMpGamerTagVisibility(GamerTagId, 9, false)
-            elseif IsPedSittingInVehicle(Ped, CurrentVehicle) and GetPedInVehicleSeat(CurrentVehicle, 0) == PlayerPedId() then
-                SetMpGamerTagVisibility(GamerTagId, 8, false)
-                SetMpGamerTagVisibility(GamerTagId, 9, true)
-            else
-                SetMpGamerTagVisibility(GamerTagId, 8, false)
-                SetMpGamerTagVisibility(GamerTagId, 9, false)
-            end
-
-            SetMpGamerTagVisibility(GamerTagId, 2, IsPlayerTargettingAnything(playerId))
-            SetMpGamerTagAlpha(GamerTagId, 2, 255)
-            SetMpGamerTagHealthBarColor(GamerTagId, 0)
-
-            SetMpGamerTagColour(GamerTagId, 0, 4)
-            SetMpGamerTagColour(GamerTagId, 4, 0)
-            SetMpGamerTagAlpha(GamerTagId, 4, 255)
-            SetMpGamerTagsUseVehicleBehavior(true)
-            SetMpGamerTagsVisibleDistance(100.0)
         end
     end
 end)
